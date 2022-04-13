e298b5956f0dda36590404b6a15e561cd08374de
Crazycoold <juandaleja@gmail.com>
2022-04-12 21:13:28 -0500

    Initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+    <div class="card">
+        <div class="card-header alert alert-info">
+            Inicia una nueva partida
+        </div>
+        <div class="card-body">
+            <div class="text-center">
+                {{ link_to_route('game.create', __('Inicia un nuevo juego'), [], ['class' => 'btn btn-outline-primary']) }}
+            </div>
+        </div>
+    </div>
+@endsection
